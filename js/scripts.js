f8b984e9195e39950f39835955775b2253bdf3e7
karaklefos <karaklefos1@gmail.com>
2025-04-09 15:01:00 -0500

    Update scripts.js
    enforcing the menu to open and close as needed

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 console.log("Welcome to My Portfolio")
+
+/* Menu Toggle */
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
