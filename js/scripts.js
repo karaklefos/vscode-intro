87aad8db7987782a040de999c079f0e26f754b36
karaklefos <karaklefos1@gmail.com>
2025-09-02 15:41:07 -0500

    hamburger menu
    removes underline from icons and adds back an X so the user can exit menu as needed

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 console.log("Welcome to My Portfolio")
 
 /* Menu Toggle */
-
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-    } else {
-        x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+  var icon = x.querySelector('.icon i'); // Select the Font Awesome icon
+
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-xmark'); // Change to X icon
+  } else {
+    x.className = 'navtoggle';
+    icon.classList.remove('fa-xmark');
+    icon.classList.add('fa-bars'); // Change back to bars
+  }
 }
