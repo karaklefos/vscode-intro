c866af7668b90eec27f67e58b513e3d308de8011
karaklefos <karaklefos1@gmail.com>
2026-01-14 21:08:13 -0600

    NEW SITE
    NEW SITE

---
+++ .next/dev/server/chunks/ssr/_next-internal_server_app_projects_life-anew_page_actions_81682973.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/.next-internal/server/app/projects/life-anew/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=_next-internal_server_app_projects_life-anew_page_actions_81682973.js.map
